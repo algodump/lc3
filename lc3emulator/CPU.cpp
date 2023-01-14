15094f6009cded233dd11ab2d3b318fc4cd84e22
Man-in-the-middl-e <dlyzno@gmail.com>
2023-01-14 15:26:46 +0200

    Implement some useful functions to retrieve instruction bits. Emulate ADD instruction.

--- lc3emulator/CPU.cpp
+++ lc3emulator/CPU.cpp
@@ -1,36 +1,97 @@
 #include "CPU.hpp"
 
+#include <assert.h>
+#include <bitset>
 #include <fstream>
 #include <iostream>
 
-bool CPU::load(const std::string& fileToRun) 
+
+namespace {
+uint16_t retrieveBits(uint16_t insturction, uint8_t start, uint8_t size)
+{
+    assert(start <= 15 && start >= 0);
+    uint16_t mask = (1 << size) - 1;
+    uint16_t res = (insturction >> (start - size + 1)) & mask;
+    return res;
+}
+
+uint16_t getDestinationRegister(uint16_t insturction)
+{
+    return retrieveBits(insturction, 11, 3);
+}
+
+uint16_t getSourceBaseRegister(uint16_t insturction)
+{
+    return retrieveBits(insturction, 8, 3);
+}
+} // namespace
+
+InsturctionOpCode CPU::getOpCode(uint16_t instruction) const
+{
+    return static_cast<InsturctionOpCode>(retrieveBits(instruction, 15, 4));
+}
+
+bool CPU::load(const std::string& fileToRun)
 {
     std::ifstream ifs(fileToRun, std::ios::binary);
     if (!ifs.is_open()) {
         return false;
     }
-
     uint16_t origin;
-    ifs.read(reinterpret_cast<char*>(&origin), sizeof(origin));
+    ifs.read(reinterpret_cast<char*>(&origin), sizeof origin);
     m_pc = origin;
 
     while (!ifs.eof()) {
         uint16_t data;
         ifs.read(reinterpret_cast<char*>(&data), sizeof(data));
-        m_memory[origin++] = data;
-    } 
+        if (!ifs.fail()) {
+            m_memory[origin++] = data;
+        }
+    }
+    // TODO: find out what are the terminating conditions
+    m_memory[origin] = 0xDEAD;
     dumpMemory(m_pc, 5);
     return true;
 }
 
 bool CPU::emulate()
 {
+    while (true) {
+        // User is responsible for not mixing data and insturctions
+        // as emulator can't differentiate insturction from
+        // raw data.
+        uint16_t instruction = m_memory[m_pc++];
+
+        if (instruction == 0xDEAD) {
+            break;
+        }
+
+        auto opCode = getOpCode(instruction);
+        switch (opCode) {
+        case InsturctionOpCode::ADD: {
+            uint8_t destinationRegister = getDestinationRegister(instruction);
+            uint8_t sourceRegister = getSourceBaseRegister(instruction);
+            if ((instruction >> 5) & 0x1) {
+                uint8_t immediateValue = retrieveBits(instruction, 4, 5);
+                m_registers[destinationRegister] =
+                    m_registers[sourceRegister] + immediateValue;
+            }
+            else {
+                uint8_t secondSource = retrieveBits(instruction, 2, 3);
+                m_registers[destinationRegister] =
+                    m_registers[sourceRegister] + m_registers[secondSource];
+            }
+            break;
+        }
+        }
+    }
     return true;
 }
 
 void CPU::dumpMemory(uint16_t start, uint16_t size)
 {
     for (uint16_t i = start; i < start + size; ++i) {
-        std::cout << "memory[ " << i << " ]" << " = " << m_memory[i] << std::endl; 
+        std::cout << "memory[ " << i << " ]"
+                  << " = " << m_memory[i] << std::endl;
     }
 }
--- lc3emulator/CPU.hpp
+++ lc3emulator/CPU.hpp
@@ -1,7 +1,35 @@
 #pragma once
 
-#include <string>
 #include <array>
+#include <string>
+
+enum class InsturctionOpCode : uint8_t {
+    BR = 0b0000,
+    ADD = 0b0001,
+    LD = 0b0010,
+    ST = 0b0011,
+    JSSR = 0b0100,
+    AND = 0b0101,
+    LDR = 0b0110,
+    STR = 0b0111,
+    RTI = 0b1000,
+    NOT = 0b1001,
+    LDI = 0b1010,
+    STI = 0b1011,
+    RET = 0b1100,
+    NON = 0b1101,
+    LEA = 0b1110,
+    TRAP = 0b1111,
+};
+
+class Instruction {
+  public:
+    Instruction(uint16_t instruction) : m_instruction(instruction) {}
+    uint16_t getBits(uint8_t from, uint8_t to);
+
+  private:
+    uint16_t m_instruction;
+};
 
 class CPU {
   public:
@@ -9,14 +37,16 @@ class CPU {
     bool load(const std::string& fileToRun);
     bool emulate();
 
-    private:
+  private:
     void dumpMemory(uint16_t start, uint16_t size);
+    InsturctionOpCode getOpCode(uint16_t instruction) const;
 
   private:
-    static constexpr uint16_t MEMORY_CAPACITY = std::numeric_limits<uint16_t>::max();
+    static constexpr uint16_t MEMORY_CAPACITY =
+        std::numeric_limits<uint16_t>::max();
     static constexpr uint8_t NUMBER_OF_REGISTERS = 8;
 
-private:    
+  private:
     std::array<uint16_t, MEMORY_CAPACITY> m_memory;
     std::array<uint8_t, NUMBER_OF_REGISTERS> m_registers;
     uint16_t m_pc;
