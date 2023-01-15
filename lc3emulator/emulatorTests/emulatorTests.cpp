dca5da7f790c5d4e944a542d6b495ba0544f9519
Man-in-the-middl-e <dlyzno@gmail.com>
2023-01-15 13:54:46 +0200

    Add instruction builder for lc3emulator tests. It helps to write more expressable, easy to change and understand tests. Rewrote all existing tests to use instruction builder.

--- lc3emulator/emulatorTests/emulatorTests.cpp
+++ lc3emulator/emulatorTests/emulatorTests.cpp
@@ -1,138 +1,204 @@
+#include "../CPU.hpp"
+#include <bitset>
 #include <gtest/gtest.h>
 
-#include "../CPU.hpp"
 
-namespace 
-{
-    uint16_t RESET_PC = 0;
-    uint16_t INIT_PC = 42;
-}
+namespace {
+uint16_t RESET_PC = 0;
+uint16_t INIT_PC = 42;
 
-// TODO: add instruction builder that allows to
-//       construct instruction from string
-class CPUTests : public ::testing::Test {
-      protected:
-        void testAddInstruction()
-        {
-            // ADD R0, R1, R2 ; R0 = R1 + R2
-            uint16_t instruction = 0b0001000001000010;
-            cpu.m_registers[R1] = 31;
-            cpu.m_registers[R2] = 42;
-            cpu.emulate(instruction);
-            ASSERT_EQ(cpu.m_registers[R0], cpu.m_registers[R1] + cpu.m_registers[R2]);
-            ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
+// copied from lc3assembler/instructions.hpp
+class InstructionBuilder {
+  public:
+    InstructionBuilder() : m_bitPointer(15) {}
+    InstructionBuilder& set(const std::string& bits)
+    {
+        for (auto bit : bits) {
+            m_instruction.set(m_bitPointer--, (bit - '0') == 1);
         }
+        return *this;
+    }
 
-        void testAndInstruction()
-        {
-            // AND R0, R1, #7; R0 = R1 & R2
-            uint16_t instruction = 0b0101000001100111;
-            cpu.m_registers[R1] = 0b0111;
-            cpu.emulate(instruction);
-            ASSERT_EQ(cpu.m_registers[R0],
-                      cpu.m_registers[R1] & 0x7);
-            ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
+    InstructionBuilder& set(Register registerNumber)
+    {
+        for (auto bit : std::bitset<3>(registerNumber).to_string()) {
+            m_instruction.set(m_bitPointer--, (bit - '0') == 1);
         }
+        return *this;
+    }
 
-        void testBrInsturction()
-        {
-            uint16_t intialPCValue = 42;
-            cpu.m_pc = intialPCValue;
-            uint16_t instructionBRnzp = 0b0000000001000000;
-            cpu.emulate(instructionBRnzp);
-            ASSERT_EQ(cpu.m_pc, intialPCValue + 64);
+    InstructionBuilder& set(char bit)
+    {
+        m_instruction.set(m_bitPointer--, (bit - '0') == 1);
+        return *this;
+    }
 
-            // N = 0 and n = 1, don't jump
-            cpu.m_pc = intialPCValue;
-            uint16_t instructionBRn = 0b0000100001000000;
-            cpu.emulate(instructionBRn);
-            ASSERT_EQ(cpu.m_pc, intialPCValue);
+    uint16_t build() const { return m_instruction.to_ulong(); }
 
-            // both N = 1 and n = 1, so jump
-            cpu.m_conditionalCodes.N = 1;
-            cpu.m_pc = intialPCValue;
-            uint16_t instructionBRnN = 0b0000100001000000;
-            cpu.emulate(instructionBRnN);
-            ASSERT_EQ(cpu.m_pc, intialPCValue + 64);
-
-            // TODO: add more test if needed
-        }
-
-        void testLdInstruction() 
-        {
-            // LD R1, VALUE
-            // value is encoded as lable location - pc
-            uint16_t value = 42;
-            cpu.m_pc = RESET_PC;
-            cpu.m_memory[R2] = value;
-            uint16_t instruction = 0b0010001000000010;
-            cpu.emulate(instruction);
-            ASSERT_EQ(cpu.m_registers[R1], value);
-            ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
-        }
-
-        void testJMP_RETInsturctions()
-        {
-            uint16_t registerValue = 42;
-            cpu.m_registers[R7] = registerValue;
-            // JMP R3
-            uint16_t jmpInstruction = 0b1100000111000000;
-            cpu.emulate(jmpInstruction);
-            ASSERT_EQ(cpu.m_pc, registerValue);
-        }
-
-        void testJSR_JSRRInstructions()
-        {
-            cpu.m_pc = INIT_PC;
-            // test JSR
-            uint16_t jsrInstruction = 0b0100100000010000;
-            cpu.emulate(jsrInstruction);
-            ASSERT_EQ(cpu.m_registers[R7], INIT_PC);
-            ASSERT_EQ(cpu.m_pc, INIT_PC + 16);
-
-            // test JSRR
-            cpu.m_pc = INIT_PC;
-            cpu.m_registers[R2] = 31;
-            uint16_t jsrrInstruction = 0b0100000010000000;
-            cpu.emulate(jsrrInstruction);
-            ASSERT_EQ(cpu.m_registers[R7], INIT_PC);
-            ASSERT_EQ(cpu.m_pc, cpu.m_registers[R2]);
-        }
-
-      protected:
-        CPU cpu;
+  private:
+    uint8_t m_bitPointer;
+    std::bitset<16> m_instruction;
 };
 
-TEST_F(CPUTests, AddInstruction)
+template <uint16_t bitcount = 9>
+static std::string toBinaryString(uint16_t number)
 {
-    testAddInstruction();
+    return std::bitset<bitcount>(number).to_string();
 }
+} // namespace
 
-TEST_F(CPUTests, AndInstruction)
-{
-    testAndInstruction();
-}
+class CPUTests : public ::testing::Test {
+  protected:
+    void testAddInstruction()
+    {
+        // ADD R0, R1, R2 ; R0 = R1 + R2
+        uint16_t addInstruction = InstructionBuilder()
+                                      .set("0001")
+                                      .set(R0)
+                                      .set(R1)
+                                      .set("000")
+                                      .set(R2)
+                                      .build();
+        cpu.m_registers[R1] = 31;
+        cpu.m_registers[R2] = 42;
+        cpu.emulate(addInstruction);
+        ASSERT_EQ(cpu.m_registers[R0],
+                  cpu.m_registers[R1] + cpu.m_registers[R2]);
+        ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
+    }
 
-TEST_F(CPUTests, LdInstruction)
-{
-    testLdInstruction();
-}
+    void testAndInstruction()
+    {
+        // AND R0, R1, #7; R0 = R1 & R2
+        uint16_t immediateValue = 7;
+        uint16_t andInstruction = InstructionBuilder()
+                                      .set("0101")
+                                      .set(R0)
+                                      .set(R1)
+                                      .set("1")
+                                      .set(toBinaryString<5>(immediateValue))
+                                      .build();
+        cpu.m_registers[R1] = 8;
+        cpu.emulate(andInstruction);
+        ASSERT_EQ(cpu.m_registers[R0], cpu.m_registers[R1] & immediateValue);
+        ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
+    }
 
-TEST_F(CPUTests, BrInstruction)
-{
-    testBrInsturction();
-}
+    void testBrInsturction()
+    {
+        cpu.m_pc = INIT_PC;
+        uint16_t offset = 64;
+        uint16_t instructionBRnzp = InstructionBuilder()
+                                        .set("0000")
+                                        .set("000")
+                                        .set(toBinaryString(offset))
+                                        .build();
+        cpu.emulate(instructionBRnzp);
+        ASSERT_EQ(cpu.m_pc, INIT_PC + offset);
 
-TEST_F(CPUTests, JMP_RETInstructions)
-{
-    testJMP_RETInsturctions();
-}
+        // N = 0 and n = 1, don't jump
+        cpu.m_pc = INIT_PC;
+        uint16_t instructionBRn = InstructionBuilder()
+                                      .set("0000")
+                                      .set("100")
+                                      .set(toBinaryString(offset))
+                                      .build();
+        cpu.emulate(instructionBRn);
+        ASSERT_EQ(cpu.m_pc, INIT_PC);
 
-TEST_F(CPUTests, JSR_JSRRInstructions)
-{
-    testJSR_JSRRInstructions();
-}
+        // // both N = 1 and n = 1, so jump
+        cpu.m_conditionalCodes.N = 1;
+        cpu.m_pc = INIT_PC;
+        uint16_t instructionBRnN = InstructionBuilder()
+                                       .set("0000")
+                                       .set("100")
+                                       .set(toBinaryString(offset))
+                                       .build();
+        cpu.emulate(instructionBRnN);
+        ASSERT_EQ(cpu.m_pc, INIT_PC + offset);
 
+        // TODO: add more test if needed
+    }
+
+    void testLdInstruction()
+    {
+        // LD R1, VALUE
+        // value is encoded as lable location - pc
+        //
+        uint16_t offset = 1;
+        uint16_t value = 42;
+        cpu.m_pc = RESET_PC;
+        cpu.m_memory[offset] = value;
+
+        uint16_t instruction = InstructionBuilder()
+                                   .set("0010")
+                                   .set(R1)
+                                   .set(toBinaryString(offset))
+                                   .build();
+        cpu.emulate(instruction);
+        ASSERT_EQ(cpu.m_registers[R1], value);
+        ASSERT_EQ(cpu.m_conditionalCodes.P, 1);
+    }
+
+    void testJMP_RETInsturctions()
+    {
+        uint16_t registerValue = 42;
+        cpu.m_registers[R7] = registerValue;
+        // JMP R3
+        uint16_t jmpInstruction = InstructionBuilder()
+                                      .set("1100")
+                                      .set("000")
+                                      .set(R7)
+                                      .set("000000")
+                                      .build();
+        ;
+        cpu.emulate(jmpInstruction);
+        ASSERT_EQ(cpu.m_pc, registerValue);
+    }
+
+    void testJSR_JSRRInstructions()
+    {
+        cpu.m_pc = INIT_PC;
+        uint16_t offset = 16;
+        // test JSR
+        uint16_t jsrInstruction = InstructionBuilder()
+                                      .set("0100")
+                                      .set('1')
+                                      .set(toBinaryString<11>(offset))
+                                      .build();
+        cpu.emulate(jsrInstruction);
+        ASSERT_EQ(cpu.m_registers[R7], INIT_PC);
+        ASSERT_EQ(cpu.m_pc, INIT_PC + offset);
+
+        // test JSRR
+        cpu.m_pc = INIT_PC;
+        cpu.m_registers[R2] = 31;
+        uint16_t jsrrInstruction = InstructionBuilder()
+                                       .set("0100")
+                                       .set("000")
+                                       .set(R2)
+                                       .set("000000")
+                                       .build();
+        cpu.emulate(jsrrInstruction);
+        ASSERT_EQ(cpu.m_registers[R7], INIT_PC);
+        ASSERT_EQ(cpu.m_pc, cpu.m_registers[R2]);
+    }
+
+  protected:
+    CPU cpu;
+};
+
+TEST_F(CPUTests, AddInstruction) { testAddInstruction(); }
+
+TEST_F(CPUTests, AndInstruction) { testAndInstruction(); }
+
+TEST_F(CPUTests, LdInstruction) { testLdInstruction(); }
+
+TEST_F(CPUTests, BrInstruction) { testBrInsturction(); }
+
+TEST_F(CPUTests, JMP_RETInstructions) { testJMP_RETInsturctions(); }
+
+TEST_F(CPUTests, JSR_JSRRInstructions) { testJSR_JSRRInstructions(); }
 
 int main(int argc, char* argv[])
 {
